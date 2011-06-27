b63c7d5a684a800145521f13e81360162466ed66
Jason Jarrett <jason@elegantcode.com>
2011-06-27 14:43:46 -0700

    added a test

--- DumpToText/Tests/ReferenceObjectTests.cs
+++ DumpToText/Tests/ReferenceObjectTests.cs
@@ -5,18 +5,24 @@ namespace DumpToText.Tests
 {
 	public class ReferenceObjectTests
 	{
-		public class SimpleFoo
-		{
-			public int Foo { get; set; }
-		}
+
 		[Test]
 		public void SimpleReferenceObject()
 		{
-			var foo = new SimpleFoo { Foo = 1 };
+			var foo = new { Foo = 1 };
 			var dumpItemBase = ObjectTypeFactory.Create(foo);
 			dumpItemBase.ShouldBeOfType(typeof (ReferenceObject));
 
 			dumpItemBase.Children.Count().ShouldEqual(1);
+		}
+
+		[Test]
+		public void SimpleRefWithCollectionProperty()
+		{
+			var foo = new {Foo = new int[0]};
+			var dumpItemBase = ObjectTypeFactory.Create(foo);
+			dumpItemBase.Children.Count().ShouldEqual(1);
+			dumpItemBase.Children.First().Children.Count().ShouldEqual(0);
 		}
 	}
 }
